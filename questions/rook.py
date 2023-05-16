bcc47313e10d4f51c0b2cff74f83fe109cc6867e
Christopher Yoazar Palacios Santos <christopher.yps@gmail.com>
2023-05-15 21:56:53 -0600

    rook restructured as a class

--- questions/rook.py
+++ questions/rook.py
@@ -1,50 +1,72 @@
-def rook_movement():
+class Rook:
     """
-    Makes the calculations of the total of spaces moved and the total distance from the start
+    Class representing the rook piece and its movement in a chess board
 
     Returns
     ------------------------------
     str:
         String describing the number of moves and the distance from the start position
     """
-    rook = [0, 0]
-    total_moves = 0
 
-    while True:
-        movement = print_menu()
-        if movement is False:
-            break
-        if movement[1] == "x":
-            rook[0] += movement[0]
-        elif movement[1] == "y":
-            rook[1] += movement[0]
-        total_moves += abs(movement[0])
-    distance = abs(rook[0]) + abs(rook[1])
-    return f"The Rook has traveled a distance of {total_moves} spaces and is {distance} spaces away from its starting point"
+    def __init__(self):
+        """
+        Starts the position of the rook at coordintates 0,0
+        Starts total moves done by the rook at 0
+        """
+        self._position = [0, 0]
+        self._total_moves = 0
 
-def print_menu():
-    """
-    Prints the menu for the user to make a move
+    @property
+    def summary_string(self):
+        """
+        Property for getting a summary string for the movements done by the rook
 
-    Returns
-    ------------------------------
-    list:
-        List with the number of spaces the rook will move and the axis of the movement
-    """
-    operator = 1
+        Returns
+        ------------------------------
+        str:
+            String describing the number of moves and the distance from the start position
+        """
+        return f"The Rook has traveled a distance of {self.total_moves} spaces and is {self.distance} spaces away from its starting point"
 
-    print("Choose a direction:")
-    print("1 - Up")
-    print("2 - Down")
-    print("3 - Left")
-    print("4 - Right")
-    print("e - Exit")
-    print("Choice: ")
-    direction = input()
+    @property
+    def distance(self):
+        """
+        Poperty for getting the total spaces traveled by the rook
 
-    if direction == "e":
-        return False
-    else:
+        Returns
+        ------------------------------
+        int:
+            Summatory of the absolute values of the coordinates to determine the distance traveled
+        """
+        return abs(self._position[0]) + abs(self._position[1])
+
+    @property
+    def total_moves(self):
+        """
+        Poperty for getting the total movement spaces done by the rook after a sequence
+
+        Returns
+        ------------------------------
+        int:
+            total moves after the sequence
+        """
+        return self._total_moves
+
+    def movement_input(self, direction, moves):
+        """
+        Take input for sequence of movement and send it to the execute_movement_command function
+        Determine the axis and real value of the movement to be done
+
+        Parameters
+        ------------------------------
+        direction: str
+            Direction for the piece to move, used to calculate real value and axis
+
+        moves: str
+            moves to be done in the present movement, used to calculate real value
+        """
+        operator = 1
+        axis = ''
         if direction == "2" or direction == "3":
             operator = -1
         if direction == "1" or direction =="2":
@@ -52,11 +74,48 @@ def print_menu():
         elif direction == "3" or direction =="4":
             axis = "x"
 
-    print("Insert number of spaces to move (input e to exit): ")
-    moves = input()
-    if moves == "e":
-        return False
-    return [int(moves) * operator, axis]
+        movement_command = [int(moves) * operator, axis]
+        self.execute_movement_command(movement_command)
+
+    def execute_movement_command(self, movement_command):
+        """
+        Calculate the movement in the coordinates according to the real value and the axis.
+        Calculate the total moves using the absolute number of movements
+
+        Parameters
+        ------------------------------
+        movement_command: list
+            List containing the real value for the movement and the axis to apply the calculation to.
+        """
+        if movement_command[1] == "x":
+            self._position[0] += movement_command[0]
+        elif movement_command[1] == "y":
+            self._position[1] += movement_command[0]
+        self._total_moves += abs(movement_command[0])
+
 
 if __name__ == "__main__":
-    print( rook_movement() )
+    rook = Rook()
+
+    while True:
+        print("Choose a direction:")
+        print("1 - Up")
+        print("2 - Down")
+        print("3 - Left")
+        print("4 - Right")
+        print("e - Exit")
+        print("Choice: ")
+
+        direction = input()
+        if direction == "e":
+            break
+
+        print("Insert number of spaces to move (input e to exit): ")
+
+        moves = input()
+        if moves == "e":
+            break
+
+        rook.movement_input(direction, moves)
+
+    print(rook.summary_string)
